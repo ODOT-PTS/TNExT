db4729b63326520ef3a9947141667e4da4227e82
mohsenis <mohseni.s.alireza@gmail.com>
2016-08-23 18:35:29 -0700

    Commit 17. disabled connected grapg

--- TNAtoolAPI-Webapp/src/main/webapp/resources/js/client.js
+++ TNAtoolAPI-Webapp/src/main/webapp/resources/js/client.js
@@ -1727,4 +1727,4 @@ function updateListDialog(agenciesIds){
 /*
  * Connectivity Graph
  */
-$('#map > div.leaflet-control-container > div.leaflet-top.leaflet-left').append('<div id="con-graph-control"  class="leaflet-control ui-widget-content" style="border-radius:5px; border:0"><button id="con-graph-button" style="border-radius:5px; background-color:#FFF" onclick="toggleConGraphDialog()">G</button></div>');
+//$('#map > div.leaflet-control-container > div.leaflet-top.leaflet-left').append('<div id="con-graph-control"  class="leaflet-control ui-widget-content" style="border-radius:5px; border:0"><button id="con-graph-button" style="border-radius:5px; background-color:#FFF" onclick="toggleConGraphDialog()">G</button></div>');
