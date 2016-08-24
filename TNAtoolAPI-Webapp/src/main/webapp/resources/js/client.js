4cc944e8b864c14e9d755670dc8008f7720bbf10
mohsenis <mohseni.s.alireza@gmail.com>
2016-08-23 18:36:13 -0700

    Commit 17.

--- TNAtoolAPI-Webapp/src/main/webapp/resources/js/client.js
+++ TNAtoolAPI-Webapp/src/main/webapp/resources/js/client.js
@@ -1726,5 +1726,5 @@ function updateListDialog(agenciesIds){
 
 /*
  * Connectivity Graph
- */
+ */ 
 //$('#map > div.leaflet-control-container > div.leaflet-top.leaflet-left').append('<div id="con-graph-control"  class="leaflet-control ui-widget-content" style="border-radius:5px; border:0"><button id="con-graph-button" style="border-radius:5px; background-color:#FFF" onclick="toggleConGraphDialog()">G</button></div>');
