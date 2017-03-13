f9071ea7773a87e580d369cc910a1940cda8319c
mohsenis <mohseni.s.alireza@gmail.com>
2017-03-13 12:14:50 -0700

    Fixed some spelling

--- TNAtoolAPI-Webapp/src/main/webapp/resources/admin/adminTool.js
+++ TNAtoolAPI-Webapp/src/main/webapp/resources/admin/adminTool.js
@@ -455,7 +455,7 @@ function checkForDeactivated(){
 function activateDBs(index){
 	if(!dbStatus[index].GtfsFeeds || !dbStatus[index].Census || !dbStatus[index].Employment 
 			|| !dbStatus[index].Parknride || !dbStatus[index].Title6 || !dbStatus[index].FutureEmp || !dbStatus[index].FuturePop || !dbStatus[index].Region){
-		alert("Comlete all the datasets before activatig the database.");
+		alert("Complete all the data sources before activating the database.");
 		return;
 	}
 	if(!dbStatus[index].Updated){
@@ -2072,8 +2072,8 @@ $(document).ready(function(){
             			"<td><img src='../resources/images/check.png' alt='dataset status' style='width: 1.2em;margin-left: 0.3em;' class='update'></td></tr>"+
             			"<tr><td><input type='button' class='activate dbButtons-class no_css single' value='Activate Database' style='background-color: rgba(4, 128, 0, 0.48);' onclick='activeDeactive("+i+")'></td>"+
             			"<td><img src='../resources/images/check.png' alt='dataset status' style='width: 1.2em;margin-left: 0.3em;' class='activate'></td></tr>"+
-            			"<tr><td><input type='button' class='modify dbButtons-class no_css single' value='Modify Database Information' style='background-color:rgba(128, 87, 0, 0.46)' onclick='addModifyDB("+i+", "+dbInfo[i][0]+")'></td>"+
-            			"<td></td></tr>"+
+            			/*"<tr><td><input type='button' class='modify dbButtons-class no_css single' value='Modify Database Information' style='background-color:rgba(128, 87, 0, 0.46)' onclick='addModifyDB("+i+", "+dbInfo[i][0]+")'></td>"+
+            			"<td></td></tr>"+*/
             			"<tr><td><input type='button' class='delete dbButtons-class no_css single' value='Delete Database' style='background-color:rgba(205, 10, 10, 0.65)' onclick='deleteDb("+dbInfo[i][0]+")'></td>"+
             			"<td></td></tr>"+
             			"</table></div></td>";
