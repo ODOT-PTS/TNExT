e2af086805933dd7ab2d12b69c4c50877f50bc86
pbarahimi <pbarahimi@gmail.com>
2017-03-15 08:05:18 -0700

    Typo fix in Flexible Report table. Implementing metrics definition tree.

--- TNAtoolAPI-Webapp/src/main/webapp/resources/js/metrics-definition.js
+++ TNAtoolAPI-Webapp/src/main/webapp/resources/js/metrics-definition.js
@@ -19,6 +19,138 @@ var metricDef = [
 			"metric" : "Total Stops",
 			"definition" : "Total number of stops within the given geographic area."
 		},
+		{
+			"report" : "Transit Agencies Summary Report",
+			"metric" : "Geographic Areas",
+			"definition" : "Count of geographic areas that the transit agency operates within."
+		},
+		{
+			"report" : "Transit Agencies Summary Report",
+			"metric" : "Total Counties",
+			"definition" : "Count of counties that the transit agency operates within."
+		},
+		{
+			"report" : "Transit Agencies Summary Report",
+			"metric" : "Total Census Places",
+			"definition" : "Count of census places that the transit agency operates within."
+		},
+		{
+			"report" : "Transit Agencies Summary Report",
+			"metric" : "Total Urban Areas",
+			"definition" : "Count of urban areas that the transit agency operates within."
+		},
+		{
+			"report" : "Transit Agencies Summary Report",
+			"metric" : "Total Congressional Districts",
+			"definition" : "Count of congressional districts that the transit agency operates within."
+		},
+		{
+			"report" : "Transit Agencies Summary Report",
+			"metric" : "Total ODOT Transit Regions",
+			"definition" : "Count of ODOT transit regions that the transit agency operates within."
+		},
+		{
+			"report" : "Transit Agencies Summary Report",
+			"metric" : "Fare",
+			"definition" : "If available, points to the fare information published by the transit agency on its web site."
+		},
+		{
+			"report" : "Transit Agencies Summary Report",
+			"metric" : "Service Start Date",
+			"definition" : "The earliest service date specified in the transit agency feed in YYYYMMDD format."
+		},
+		{
+			"report" : "Transit Agencies Summary Report",
+			"metric" : "Service End Date",
+			"definition" : "The latest service date specified in the transit agency feed in YYYYMMDD format."
+		},
+		
+		{
+			"report" : "Transit Agencies Summary Report",
+			"metric" : "Feed Information",
+			"definition" : "If available, this field points to the feed information such as name, version, publisher name and publisher URL."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Route Miles",
+			"definition" : "Summation of the lengths of the longest route variant for all routes that the transit agency serves."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Route Stops",
+			"definition" : "Total number of stops served by the transit agency."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Stops Per Route Mile",
+			"definition" : "Number of stops in the agency\'s routes divided by Route Miles."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Service Hours",
+			"definition" : "Total hours the transit agency spends on serving all round trips of routes. Service hours may be calculated for a specific date or a set of dates specified using the calendar. Reported number are cumulative over the selected dates."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Service Miles",
+			"definition" : "Total miles driven by a transit agency over all round trips of a route. Service miles may be calculated for a specific date or a set of dates specified using the calendar. Reported number are cumulative over the selected dates."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Urban Population Served",
+			"definition" : "Summation of unduplicated urban population within X-mile radius (i.e., stop distance) of all stops that the transit agency serves."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Rural Population Served",
+			"definition" : "Summation of unduplicated rural population within X-mile radius (i.e., stop distance) of all stops that the transit agency serves."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Employment (RAC) Served",
+			"definition" : "Summation of  Employment (RAC) within X-mile radius (i.e., stop distance) of all stops that the transit agency serves."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Employees (WAC) Served",
+			"definition" : "Summation of Employees(WAC) within X-mile radius (i.e., stop distance) of all stops that the transit agency serves."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Service Stops",
+			"definition" : "Number of trips scheduled at a stop in a route. The service stops for a route is calculated as its stop count multiplied by the number of visits per stop. Reported number are cumulative over the selected dates."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Urban Population Served By Service",
+			"definition" : "Total unduplicated urban population impacted within an X-mile radius (i.e., stop distance) of all stops that the transit agency serves. Urban population served by service is calculated as service stops multiplied by the unduplicated urban population within an X-mile radius (i.e., stop distance) of all stops that the transit agency serves. Reported number is cumulative over the selected dates."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Rural Population Served By Service",
+			"definition" : "Total unduplicated rural population impacted within an X-mile radius (i.e., stop distance) of all stops that the transit agency serves. Rural population served by service is calculated as service stops multiplied by the unduplicated rural population within an X-mile radius (i.e., stop distance) of all stops that the transit agency serves. Reported number is cumulative over the selected dates."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Employment (RAC) Served By Service",
+			"definition" : "Total Employment (RAC) impacted within an X-mile radius (i.e., stop distance) of all stops that the transit agency serves. Employment (RAC) served by service is calculated as service stops multiplied by the unduplicated Employment (RAC) within an X-mile radius (i.e., stop distance) of all stops that the transit agency serves. Reported number is cumulative over the selected dates."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Employees (WAC) Served By Service",
+			"definition" : "Total Employees (WAC)  impacted within an X-mile radius (i.e., stop distance) of all stops that the transit agency serves. Employees (WAC) served by service is calculated as service stops multiplied by the unduplicated Employees (WAC) within an X-mile radius (i.e., stop distance) of all stops that the transit agency serves. Reported number is cumulative over the selected dates."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Service Days",
+			"definition" : "Set of days (from the selected days) in which at least one trip is served by the selected transit agency."
+		},
+		{
+			"report" : "Transit Agency Extended Report",
+			"metric" : "Hours of Service",
+			"definition" : "Earliest and latest arrival and departure times of all stops served by the transit agency."
+		},
+		
 ];
 
 var metricsDic = {};
@@ -47,8 +179,16 @@ $.each(Object.keys(metricsDic),function(index,key){
 	})
 	if (index+1 < Object.keys(metricsDic).length) metricsJson += ',';
 	else metricsJson += '\n]}';
-})
-//metricsJson += ']}'
-console.log(metricsJson);
+});
 
-
+reportsJson = '{ "name": "Reports" , "children" : [\n';
+$.each(Object.keys(reportsDic),function(index,key){
+	reportsJson += '{ "name": "' + key + '", "children" : [\n';
+	$.each(reportsDic[key],function(ind, metric){
+		reportsJson += '{ "name" : "' + metric.metric +  '", "children" :[{"name" : "' + metric.definition + '"}]';
+		if (ind+1 == reportsDic[key].length) reportsJson += "}\n]}\n";
+		else reportsJson += "},\n";
+	})
+	if (index+1 < Object.keys(reportsDic).length) reportsJson += ',';
+	else reportsJson += '\n]}';
+});
--- TNAtoolAPI-Webapp/src/main/webapp/resources/wiki/features.md
+++ TNAtoolAPI-Webapp/src/main/webapp/resources/wiki/features.md
@@ -2,64 +2,9 @@ Features List
 =========
 
 This is a list of all features implemented in the most current version of the Transit Network Analysis Software Tool (TNAST).
+Group features by metrics name <select onchange="$('iframe').attr('src', '../../TNAtoolAPI-Webapp/Metrics.html?&flag='+$(this).val());">
+	<option value="metricsJson">Metrics</option>
+	<option value="reportsJson">Reports</option>	
+	</select>
 
-|Feature/Feature Group|Implementation Phase|
-|:------------- |-------------:|
-|_**METRICS FOR OREGON COMMUNITIES REPORTER**_||
-|&#8227;	Service Stops (stops per route per day)     |1|
-|&#8227;	Stops per square mile|2|
-|&#8227;	Service miles per square mile|2|
-|&#8227;	Miles of service per capita|2|
-|&#8227;	% of population served|2|
-|&#8227;	Stops per service mile|2|
-|&#8227;	Average fare|2|
-|&#8227;	Median fare|2|
-|&#8227;	Number of directly connected communities|2|
-|_**REPORTING FUNCTIONALITY**_||
-|&#8227;	Comparison Over Time|2|
-|_**GEOGRAPHIC AREA(S) REPORTS**_||
-|&#8227;	State|2|
-|&#8227;	Urban / rural Areas|2|
-|&#8227;	Counties|2|
-|&#8227;	Census Places|2|
-|&#8227;	Census Tracts|2|
-|&#8227;	State/Fed congressional districts|2|
-|&#8227;	ODOT transit regions|2|
-|&#8227;	Stop count|2|
-|&#8227;	Service miles|2|
-|&#8227;	Route miles|2|
-|&#8227;	Route stops|2|
-|&#8227;	Service stops|2|
-|&#8227;	Service hours|2|
-|&#8227;	Population within x distance of a route|2|
-|&#8227;	Service Stops|2|
-|&#8227;	Population Served|2|
-|&#8227;	Service days|2|
-|&#8227;	Hours of service|2|
-|&#8227;	Un-served population|2|
-|&#8227;	Population within x distance of a stop (both duplicate and unique) & within specified geographic area|2|
-|_**TRANSIT AGENCY(S)/SERVICES**_||
-|&#8227;	Stops|1|
-|&#8227;	Service miles|1|
-|&#8227;	Route miles|1|
-|&#8227;	Population within x distance of a stop (both duplicate and unique)|1|
-|&#8227;	Population within x distance of a route|1|
-|&#8227;	Service Stops|1|
-|&#8227;	Service Population|1|
-|&#8227;	Route Stops|1|
-|&#8227;	Stops per route mile|1|
-|&#8227;	Service day selection|1|
-|&#8227;	Service hours|2|
-|&#8227;	Service days|2|
-|&#8227;	Hours of service|2|
-|&#8227;	Geographic area(s) served|2|
-|&#8227;	Schedule tables|2|
-|_**MAP DISPLAY**_||
-|&#8227;	Stops|1|
-|&#8227;	Routes|1|
-|&#8227;	Routes/stops of specified agencies|1|
-|&#8227;	Population value within x distance of a stop|2|
-|&#8227;	Route run frequency|2|
-|&#8227;	Service Stops|2|
-
-
+<iframe src="../../TNAtoolAPI-Webapp/Metrics.html?&flag=metricsJson" width="1000" height="1000" frameborder="0" scrolling="no"  style="margin:0 auto;display:block;float:left;"></iframe> 
