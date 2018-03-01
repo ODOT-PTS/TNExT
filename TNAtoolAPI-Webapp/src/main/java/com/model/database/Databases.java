5352a9ddd6a9a7729ee41dfcecfaee6539e21a3e
Ian Rees <ian@ianrees.net>
2018-03-01 14:34:02 -0800

    Finished merging with DatabaseConfig

--- TNAtoolAPI-Webapp/src/main/java/com/model/database/Databases.java
+++ TNAtoolAPI-Webapp/src/main/java/com/model/database/Databases.java
@@ -18,44 +18,5 @@
 
 package com.model.database;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.collections.iterators.EntrySetMapIterator;
-
 public class Databases {
-	public static HashMap<String, String[]> infoMap = getDbInfo();
-
-	// getDbInfo
-	public static HashMap<String, String[]> getDbInfo() {
-		HashMap<String, String[]> infoMap = DatabaseConfig.toInfoMap();
-		System.out.println("The number of databases in dbInfo.csv is is: " + String.valueOf(infoMap.get("databaseIndex").length));
-		return infoMap;
-	}
-	
-	public static void updateDbInfo(boolean b) {
-  	System.err.format("Databases::updateDbInfo() called.\n"); //Ed 2017-09-12 for logging xml use.
-	}
-
-	public static void deactivateDB(int i){
-		// todo ian: remove from DatabaseConfig and call getDbInfo again
-		String[] newElement;
-		for(Map.Entry<String, String[]> entry : infoMap.entrySet()) {
-			newElement = new String[entry.getValue().length-1];
-			for(int j=0;j<i;j++){
-				newElement[j] = entry.getValue()[j];
-			}
-			for(int j=i;j<entry.getValue().length-1;j++){
-				newElement[j] = entry.getValue()[j+1];
-			}
-			infoMap.put(entry.getKey(), newElement);
-		}
-	}
 }
