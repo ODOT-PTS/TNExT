ffb17b42928ab36ee8f0c489dc777ad494b10d2d
Ian Rees <ian@ianrees.net>
2018-03-22 20:38:36 -0700

    Use preparedStatement

--- TNAtoolAPI-Webapp/src/main/java/com/webapp/api/Queries.java
+++ TNAtoolAPI-Webapp/src/main/java/com/webapp/api/Queries.java
@@ -27,6 +27,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
